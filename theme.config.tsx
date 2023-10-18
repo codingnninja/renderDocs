ab7e88892f423d96a978bb894cd060f9062e22ef
ayobami_ogundiran <ayobami_ogundiran@yahoo.com>
2023-10-17 17:28:25 -0700

    first commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span><Image width={100} height={20} alt='render logo' src='/renderlogo.png' /></span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/codingnninja/renderDocs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '$render docs | Copyright @ 2023 ',
   },
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – SWR'
+      }
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="$render" />
+      <meta property="og:description" content="JSX in the browser without virtual DOM" />
+    </>
+  )
 }
 
 export default config
