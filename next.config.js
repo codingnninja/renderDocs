5022e0cd488a0e89bebd2855be9b9496649fb695
ayobami_ogundiran <ayobami_ogundiran@yahoo.com>
2023-10-20 15:34:05 -0700

    add steps to gallary

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ const withNextra = require('nextra')({
   output: 'export',
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  "headers": [
+    {
+      "key": "Cache-Control",
+      "value": "s-maxage=1, stale-while-revalidate=59"
+    }
+  ]
 })
 
 module.exports = withNextra()
